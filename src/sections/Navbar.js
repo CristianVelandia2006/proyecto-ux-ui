d5a7fd410c38c6760281789c6f11dfc1484c0e9e
Cristian velandia <cristianvelandia200@gmail.com>
2026-02-26 22:42:29 -0500

    Agregue el nav

---
+++ src/sections/Navbar.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './Navbar.css';
+
+const Navbar = () => {
+    return (
+        <nav className="navbar-container">
+            <div className="navbar-logo">
+                <img src="/rombo.png" className="navbar-logo-img" alt="Logo Barbería" />
+                <h1 className="h1-navbar">BARBERÍA</h1>
+            </div>
+            <ul className="navbar-links">
+                <li><a href="#servicios">SERVICIOS</a></li>
+                <li><a href="#barberos">BARBEROS</a></li>
+                <li><a href="#galeria">GALERÍA</a></li>
+                <li><a href="#contacto">CONTACTO</a></li>
+                <li><button className="navbar-button" id="btn-iniciar-sesion">INICIAR SESIÓN</button></li>
+                <li><button className="navbar-button" id="btn-reservar">RESERVAR CITA</button></li>
+            </ul>
+        </nav>
+    );
+};
+
+export default Navbar;
